7b72166753fb105132481ce041117f120c0b500e
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-11-01 10:19:02 -0600

    Update get_all.js

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,6 +1,5 @@
 function getAll(){
     var request = new XMLHttpRequest;
-    //request.open('GET',"http://localhost:8000/contactos");
     request.open('GET',"https://sqlitecontactos-60aff75a79ae.herokuapp.com/contactos");
     request.send();
 
@@ -17,26 +16,21 @@ function getAll(){
 
         const tbody_contactos = document.getElementById("tbody_contactos");
     
-        var tr = document.createElement("tr");
-        var td_email = document.createElement("td");
-        var td_nombre = document.createElement("td");
-        var td_telefono = document.createElement("td");
-
         for (let i = 0; i < json.length; i++) {
             var tr = document.createElement("tr");
             var td_email = document.createElement("td");
             var td_nombre = document.createElement("td");
             var td_telefono = document.createElement("td");
-        
-        td_email.innerHTML = json[1]["email"];
-        td_nombre.innerHTML = json[1]["nombre"];
-        td_telefono.innerHTML = json[1]["telefono"];
 
-        tr.appendChild(td_email);
-        tr.appendChild(td_nombre);
-        tr.appendChild(td_telefono);
+            td_email.innerHTML = json[i]["email"];
+            td_nombre.innerHTML = json[i]["nombre"];
+            td_telefono.innerHTML = json[i]["telefono"];
 
-        tbody_contactos.appendChild(tr);
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
 
+            tbody_contactos.appendChild(tr);
+        }
     };
 };
